27f749ae291dad34ef1e37ae4a94e5ab57886613
WissamMU <wessam.2003.ali@gmail.com>
2024-07-26 20:31:40 +0300

    navbar

--- webpack.config.js
+++ webpack.config.js
@@ -5,15 +5,9 @@ const path = require('path');
 module.exports = {
     entry: './src/js/index.js',
     output: {
+        publicPath: '/',
         path: path.resolve(__dirname, 'build'),
         filename: 'js/index.js',
-    },
-    devServer: {
-        static: {
-            directory: path.join(__dirname, 'public'),
-        },
-        //compress: true,
-        port: 9000,
     },
     module: {
         rules: [
@@ -30,6 +24,26 @@ module.exports = {
                     "sass-loader",
                 ],
             },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: "images",
+                        }
+                    },
+                ],
+            },
+            {
+                test: /\.html$/i,
+                loader: "html-loader",
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
         ],
     },
 
@@ -40,4 +54,15 @@ module.exports = {
             filename: "index.html",
         }),
     ],
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'build'),
+        },
+        devMiddleware: {
+            writeToDisk: true,
+        },
+        //compress: true,
+        port: 9000,
+        open: true,
+    },
 };
