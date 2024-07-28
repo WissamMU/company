b328f5e3d2dba8b53ac883e41c1d06b4573860ec
WissamMU <wessam.2003.ali@gmail.com>
2024-07-28 22:29:06 +0300

    lattest work - in progress

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,10 @@ import '@laylazi/bootstrap-rtl/dist/js/bootstrap.min.js'
 import '@laylazi/bootstrap-rtl/dist/css/bootstrap-rtl.min.css'
 import 'jquery/dist/jquery.min.js'
 import 'popper.js/dist/popper.min.js'
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
+import '@fortawesome/fontawesome-free/js/regular'
+import '@fortawesome/fontawesome-free/js/brands'
 
 
 
